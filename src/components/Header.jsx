5a243c592fe81c152ff32d82e2ed162772b6012e
Septian Adi Pratama <septianadip7998@gmail.com>
2024-09-09 11:39:19 +0700

    header showed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import banner from "../assets/img/banner-header.png";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div
+      className="h-[800px] bg-cover bg-center border-2 border-orange-400 flex"
+      style={{ backgroundImage: `url(${banner})` }}
+    >
+      <div className="w-1/2"></div>
+      <div className="absolute w-1/2 bg-[#FFF3E3] flex flex-col p-8 text-start">
+        <h4 className="text-xl font-semibold">New Arrival</h4>
+        <h1 className="text-4xl font-bold mt-2 text-yellow-700">
+          Discover Our <br /> New Collection
+        </h1>
+        <p className="mt-4 text-lg">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+          repellendus debitis eaque corporis consequatur omnis?
+        </p>
+        <button className="mt-6 p-4 bg-yellow-700 text-white">BUY NOW</button>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import furniroLogo from "../assets/img/furniro-logo.png"
-import accountIcon from "../assets/icons/account.svg"
-import searchIcon from "../assets/icons/search.svg"
-import heartIcon from "../assets/icons/heart.svg"
-import cartIcon from "../assets/icons/cart.svg"
-
+import React from "react";
+import furniroLogo from "../assets/img/furniro-logo.png";
+import accountIcon from "../assets/icons/account.svg";
+import searchIcon from "../assets/icons/search.svg";
+import heartIcon from "../assets/icons/heart.svg";
+import cartIcon from "../assets/icons/cart.svg";
 
 const Navbar = () => {
   return (
-    <div className='container mx-auto m-5 p-6 flex justify-between border-2 border-red-400'>
+    <div className="container mx-auto p-5 flex justify-between border-2 border-red-400">
       <div>
         <img src={furniroLogo} alt="furniro logo" />
       </div>
-      <div className=''>
-        <ul className='flex gap-14 font-bold text-xl'>
+      <div className="">
+        <ul className="flex gap-14 font-bold text-lg">
           <li>Home</li>
           <li>Shop</li>
           <li>About</li>
@@ -21,15 +20,23 @@ const Navbar = () => {
         </ul>
       </div>
       <div>
-        <ul className='flex gap-5'>
-          <li><img src={accountIcon} alt="account" /></li>
-          <li><img src={searchIcon} alt="search" /></li>
-          <li><img src={heartIcon} alt="heart" /></li>
-          <li><img src={cartIcon} alt="cart" /></li>
+        <ul className="flex gap-5">
+          <li>
+            <img src={accountIcon} alt="account" />
+          </li>
+          <li>
+            <img src={searchIcon} alt="search" />
+          </li>
+          <li>
+            <img src={heartIcon} alt="heart" />
+          </li>
+          <li>
+            <img src={cartIcon} alt="cart" />
+          </li>
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
