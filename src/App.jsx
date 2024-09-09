14f03f51b64a9da5857e8c4c6c71ba166422868e
Septian Adi Pratama <septianadip7998@gmail.com>
2024-09-09 11:41:34 +0700

    room showed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import Room from "./components/Room";
 
 const App = () => {
   return (
     <div className="container mx-auto border-4 border-black">
       <Navbar />
       <Header/>
+      <Room/>
     </div>
   );
 };
