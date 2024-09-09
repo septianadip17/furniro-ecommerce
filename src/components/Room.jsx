f3f708e66b5cd11657e9fe2d41a1c86bb63cae9e
Septian Adi Pratama <septianadip7998@gmail.com>
2024-09-09 11:51:32 +0700

    enough

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import dining from "../assets/img/dining.png";
+import living from "../assets/img/living.png";
+import bedroom from "../assets/img/bedroom.png";
 
 const Room = () => {
   return (
-    <div>Room</div>
-  )
-}
+    <div className="text-center p-4 m-7">
+      {/* header title */}
+      <div className="p-4">
+        <h1 className="text-3xl font-bold">Browse The Range</h1>
+        <p className="text-gray-600">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+      </div>
 
-export default Room
+      {/* room images */}
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 justify-items-center items-center">
+        <div className="flex flex-col items-center">
+          <img src={dining} alt="Dining" />
+          <h1 className="mt-2 text-lg font-semibold">Dining</h1>
+        </div>
+        <div className="flex flex-col items-center">
+          <img src={living} alt="Living" />
+          <h1 className="mt-2 text-lg font-semibold">Living</h1>
+        </div>
+        <div className="flex flex-col items-center">
+          <img src={bedroom} alt="Bedroom" />
+          <h1 className="mt-2 text-lg font-semibold">Bedroom</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Room;
