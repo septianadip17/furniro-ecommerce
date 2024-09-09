5c6982c1f8b8f8cd14283c5aa1384c32c110dd81
Septian Adi Pratama <septianadip7998@gmail.com>
2024-09-09 12:55:34 +0700

    json clear

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,29 +2,87 @@ import React from "react";
 import syltherine from "../assets/img/products/syltherine.png";
 import leviosa from "../assets/img/products/leviosa.png";
 import lolito from "../assets/img/products/lolito.png";
-import respira from "../assets/img/products/respira.png"
-import grifo from "../assets/img/products/grifo.png"
-import muggo from "../assets/img/products/muggo.png"
-import pigky from "../assets/img/products/pingky.png"
-import potty from "../assets/img/products/potty.png"
+import respira from "../assets/img/products/respira.png";
+import grifo from "../assets/img/products/grifo.png";
+import muggo from "../assets/img/products/muggo.png";
+import pigky from "../assets/img/products/pingky.png";
+import potty from "../assets/img/products/potty.png";
 
 const Products = () => {
-
   const allProducts = [
     {
-      name : "Syltherine",
-      desc : "Stylish cafe chair",
-      discPrice : "2.500.000",
-      normalPrice : "3.500.000",
+      name: "Syltherine",
+      desc: "Stylish cafe chair",
+      discPrice: "2.500.000",
+      normalPrice: "3.500.000",
       tagType: "discount",
       tagName: "-30%",
-      img: "syltherine"
+      img: "syltherine",
     },
     {
-      
-    }
-
-  ]
+      name: "Leviosa",
+      desc: "Stylish cafe chair",
+      discPrice: null,
+      normalPrice: "2.500.000",
+      tagType: null,
+      tagName: null,
+      img: "leviosa",
+    },
+    {
+      name: "Lolito",
+      desc: "Luxury big sofa",
+      discPrice: "7.000.000",
+      normalPrice: "14.000.000",
+      tagType: "discount",
+      tagName: "-50%",
+      img: "lolito",
+    },
+    {
+      name: "Respira",
+      desc: "Outdoor bar table and stool",
+      discPrice: null,
+      normalPrice: "500.000",
+      tagType: "new",
+      tagName: "New",
+      img: "respira",
+    },
+    {
+      name: "Grifo",
+      desc: "Night lamp",
+      discPrice: null,
+      normalPrice: "1.500.000",
+      tagType: null,
+      tagName: null,
+      img: "Grifo",
+    },
+    {
+      name: "Muggo",
+      desc: "Small mug",
+      discPrice: null,
+      normalPrice: "150.000",
+      tagType: "new",
+      tagName: "New",
+      img: "muggo",
+    },
+    {
+      name: "Pingky",
+      desc: "Cute bed set",
+      discPrice: "7.000.000",
+      normalPrice: "14.000.000",
+      tagType: "discount",
+      tagName: "-50%",
+      img: "pingky",
+    },
+    {
+      name: "Potty",
+      desc: "Minialist flower pot",
+      discPrice: null,
+      normalPrice: "500.000",
+      tagType: "new",
+      tagName: "New",
+      img: "potty",
+    },
+  ];
 
   return (
     <div>
@@ -34,9 +92,7 @@ const Products = () => {
       </div>
 
       {/* products grid  */}
-      <div className="grid grid-cols-4">
-
-      </div>
+      <div className="grid grid-cols-4"></div>
     </div>
   );
 };
