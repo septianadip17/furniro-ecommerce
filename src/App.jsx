57e3294c67eeb7784df168f0ab81d121323b3c89
Septian Adi Pratama <septianadip7998@gmail.com>
2024-09-09 11:14:05 +0700

    title and icon changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <div className="container mx-auto border-4 border-black">
       <Navbar />
+      <Header/>
     </div>
   );
 };
