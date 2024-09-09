a00dfcbb709af9e3d02ac66c059fab3d969da115
Septian Adi Pratama <septianadip7998@gmail.com>
2024-09-09 12:44:12 +0700

    header success position and change true color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,17 +7,19 @@ const Header = () => {
       className="h-[800px] bg-cover bg-center border-2 border-orange-400 flex"
       style={{ backgroundImage: `url(${banner})` }}
     >
-      <div className="w-1/2"></div>
-      <div className="absolute w-1/2 bg-[#FFF3E3] flex flex-col p-8 text-start">
-        <h4 className="text-xl font-semibold">New Arrival</h4>
-        <h1 className="text-4xl font-bold mt-2 text-yellow-700">
-          Discover Our <br /> New Collection
-        </h1>
-        <p className="mt-4 text-lg">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
-          repellendus debitis eaque corporis consequatur omnis?
-        </p>
-        <button className="mt-6 p-4 bg-yellow-700 text-white">BUY NOW</button>
+      <div className="flex justify-center items-center mx-auto">
+        <div className="w-1/2"></div>
+        <div className="w-1/2 bg-[#FFF3E3] flex flex-col p-8 text-start">
+          <h4 className="text-xl font-semibold">New Arrival</h4>
+          <h1 className="text-4xl font-bold mt-2 text-[#B88E2F]">
+            Discover Our <br /> New Collection
+          </h1>
+          <p className="mt-4 text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+            repellendus debitis eaque corporis consequatur omnis?
+          </p>
+          <button className="mt-6 p-4 bg-[#B88E2F] text-white">BUY NOW</button>
+        </div>
       </div>
     </div>
   );
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,20 +1,44 @@
-import React from 'react'
-
+import React from "react";
+import syltherine from "../assets/img/products/syltherine.png";
+import leviosa from "../assets/img/products/leviosa.png";
+import lolito from "../assets/img/products/lolito.png";
+import respira from "../assets/img/products/respira.png"
+import grifo from "../assets/img/products/grifo.png"
+import muggo from "../assets/img/products/muggo.png"
+import pigky from "../assets/img/products/pingky.png"
+import potty from "../assets/img/products/potty.png"
 
 const Products = () => {
+
+  const allProducts = [
+    {
+      name : "Syltherine",
+      desc : "Stylish cafe chair",
+      discPrice : "2.500.000",
+      normalPrice : "3.500.000",
+      tagType: "discount",
+      tagName: "-30%",
+      img: "syltherine"
+    },
+    {
+      
+    }
+
+  ]
+
   return (
     <div>
-      {/*  */}
-      <div>
-        <h1>Our Products</h1>
+      {/* title */}
+      <div className="text-center">
+        <h1 className="mt-2 text-3xl font-bold">Our Products</h1>
       </div>
 
-      {/*  */}
-      <div>
+      {/* products grid  */}
+      <div className="grid grid-cols-4">
 
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
