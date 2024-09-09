108133ad175eab05c3f20f2ec51dec7da21c15a0
Septian Adi Pratama <septianadip7998@gmail.com>
2024-09-09 11:52:18 +0700

    products showed

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Room from "./components/Room";
+import Products from "./components/Products";
 
 const App = () => {
   return (
     <div className="container mx-auto border-4 border-black">
       <Navbar />
-      <Header/>
-      <Room/>
+      <Header />
+      <Room />
+      <Products />
     </div>
   );
 };
