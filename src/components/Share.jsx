b8fb68a770ba974cc95bd3e7686cd9306fc168fe
Septian Adi Pratama <septianadip7998@gmail.com>
2024-09-17 19:06:59 +0700

    push

--- src/components/Share.jsx
+++ src/components/Share.jsx
@@ -3,17 +3,19 @@ import banner from "../assets/img/furniture-banner.png";
 
 const Share = () => {
   return (
-    <div
-      className="h-[750px] bg-cover bg-center flex"
-      style={{ backgroundImage: `url(${banner})` }}
-    >
-      <div className="text-center justify-center items-center mx-auto space-y-2">
-        <h2 className="text-1xl font-bold text-gray-500">
-          Share our setup with
-        </h2>
-        <h1 className="text-4xl font-bold">#FuniroFurniture</h1>
+    <>
+      <div
+        className="h-[750px] bg-cover bg-center flex"
+        style={{ backgroundImage: `url(${banner})` }}
+      >
+        <div className="text-center justify-center items-center mx-auto space-y-2">
+          <h2 className="text-1xl font-bold text-gray-500">
+            Share our setup with 
+          </h2>
+          <h1 className="text-4xl font-bold">#FuniroFurniture</h1>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
